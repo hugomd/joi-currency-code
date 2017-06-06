eb7013f7eb68d595a8dc1b677455698e942bd46e
Hugo Müller-Downing <hugo@localz.co>
2017-06-06 11:26:09 +1000

    ✨ Add Joi extension

---
+++ index.js
@@ -0,0 +1,23 @@
+const isCurrency = require('is-currency');
+
+module.exports = Joi => {
+  return Joi.extend(Joi => ({
+    name: 'string',
+    base: Joi.string(),
+    language: {
+      currency: 'needs to be a valid ISO 4217 currency code'
+    },
+    rules: [
+      {
+        name: 'currency',
+        validate(params, value, state, options) {
+          if (!isCurrency(value)) {
+            return this.createError('string.currency', {v: value}, state, options);
+          }
+
+          return value;
+        }
+      }
+    ]
+  }));
+};
