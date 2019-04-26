607ca062cdd3f0e2f664eddc2dd50982bc65148d
Andy Kvochick <Andy@Amys-iMac.domain_not_set.invalid>
2019-04-25 20:54:34 -0400

    moves index file to fix npm issue

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-module.exports = require('./src/joi-currency-code');
+const isCurrencyCode = require('is-currency-code');
+
+module.exports = Joi => {
+  return Joi.extend(Joi => ({
+    name: 'string',
+    base: Joi.string(),
+    language: {
+      currency: 'needs to be a valid ISO 4217 currency code'
+    },
+    rules: [
+      {
+        name: 'currency',
+        validate(params, value, state, options) {
+          if (!isCurrencyCode(value)) {
+            return this.createError('string.currency', {v: value}, state, options);
+          }
+
+          return value.toUpperCase();
+        }
+      }
+    ]
+  }));
+};
